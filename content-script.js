da1e53a36e8bf4e0bfa0245095dcd28cc6df24d9
supershowwei <supershowwei@gmail.com>
2023-04-22 19:15:00 +0800

    新增「名詞解釋」按鈕

--- content-script.js
+++ content-script.js
@@ -1,30 +1,55 @@
 
-const attachShortcuts = function () {
-    const $self = $(this);
-
-//     const template = `將下面的英文一字不漏地翻譯成台灣正體中文，言辭要順暢，產生 3 種版本的翻譯結果，只要翻譯就好，不要格式化顯示結果。
-
-// 英文：###
-// {{CLIPBOARDTEXT}}
-// ###`;
-    
-    const template = `假設你是一位英文翻譯的專家，請將下面的英文一字不漏地翻譯成台灣正體中文，言辭要順暢，且不要格式化顯示結果。
+const shortcuts = [{
+    name: "英翻中",
+    content: `將下面的英文一字不漏地翻譯成台灣正體中文，言辭要順暢，產生 3 種版本的翻譯結果，只要翻譯就好，不要格式化顯示結果。
 
 英文：###
 {{CLIPBOARDTEXT}}
-###`;
-    
-    const $shortcutDiv = $(`<div class="shortcut-container"></div>`);
-    const $shortcut = $(`<div class="shortcut shortcut-tooltip">英翻中<span class="shortcut-tooltip-text multiline">${template}</div>`);
+###`,
+    replacement: "{{CLIPBOARDTEXT}}"
+},
+{
+    name: "名詞解釋",
+    content: `{{CLIPBOARDTEXT}} 中的 {{_CURSOR_}} 在這裡是什麼意思？可以有幾種翻譯？`,
+    replacement: "{{CLIPBOARDTEXT}}"
+}];
 
-    $shortcut.on("click", function () {
-        navigator.clipboard.readText().then(clipText => {
-            $self.find("textarea").val(template.replace(/\{\{CLIPBOARDTEXT\}\}/g, clipText));
-            $self.find("button").enable().click();
-        });
+const attachShortcuts = function () {
+    const $self = $(this);
+
+    const $shortcutDiv = $(`<div class="shortcut-container"></div>`);
+
+    shortcuts.forEach(shortcut => {
+
+        $(`<div class="shortcut shortcut-tooltip">${shortcut.name}<span class="shortcut-tooltip-text multiline">${shortcut.content}</div>`)
+            .on("click", function () {
+                navigator.clipboard.readText().then(clipText => {
+                    const message = shortcut.replacement && shortcut.content.includes(shortcut.replacement)
+                        ? shortcut.content.replace(new RegExp(shortcut.replacement, "g"), clipText)
+                        : shortcut.content;
+
+                    const $textarea = $self.find("textarea");
+
+                    if (shortcut.content.includes("{{_CURSOR_}}")) {
+                        const position = message.indexOf("{{_CURSOR_}}");
+
+                        $textarea.val(message.replace("{{_CURSOR_}}", "")).focus();
+
+                        $textarea[0].selectionStart = position;
+                        $textarea[0].selectionEnd = position;
+
+                        $self.find("button").enable();
+                    } else {
+                        $self.find("button").enable().click();
+                    }
+
+                });
+            })
+            .appendTo($shortcutDiv);
+
     });
 
-    $self.before($shortcutDiv.append($shortcut));
+    $self.before($shortcutDiv);
 }
 
 attachShortcuts.call($("form[class*='stretch'] textarea").closest("div")[0]);
@@ -35,10 +60,10 @@ var mutationObserver = new MutationObserver(records => {
         const nodes = record.addedNodes;
 
         let found;
-        
+
         for (let j = nodes.length - 1; j >= 0; j--) {
             const $node = $(nodes[j]);
-            
+
             const $inputDiv = $node.find("form[class*='stretch'] textarea").closest("div")
 
             if ($inputDiv.length > 0) {
